33f8e754aac611739976510654fae9a5fcdf9af6
José Miguel Barros da Silva <miguelbarros1983@hotmail.com>
2017-09-27 12:29:35 +0100

    Create StringResource Update the function to check if is Valid or not And change the hard code text to string from stringResourse

---
+++ ValidationTaxPayerCard/ValidationTaxPayerCard-CLI/Functions.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace ValidationTaxPayerCard_CLI
+{
+    using System;
+    
+    public class Functions
+    {
+        public bool IsValidContrib(string Contrib)
+        {
+            bool functionReturnValue = false;
+            string[] s = new string[9];
+
+            s[0] = Convert.ToString(Contrib[0]);
+            s[1] = Convert.ToString(Contrib[1]);
+            s[2] = Convert.ToString(Contrib[2]);
+            s[3] = Convert.ToString(Contrib[3]);
+            s[4] = Convert.ToString(Contrib[4]);
+            s[5] = Convert.ToString(Contrib[5]);
+            s[6] = Convert.ToString(Contrib[6]);
+            s[7] = Convert.ToString(Contrib[7]);
+            s[8] = Convert.ToString(Contrib[8]);
+
+            if (Contrib.Length == 9)
+            {
+                string c = s[0];
+
+                if (c == null)
+                {
+                    throw new ArgumentNullException(nameof(Contrib));
+                }
+
+                if (s[0] == "1" || s[0] == "2" || s[0] == "5" || s[0] == "6" || s[0] == "9")
+                {
+                    long checkDigit = Convert.ToInt32(c) * 9;
+                    int i;
+                    for (i = 2; i <= 8; i++)
+                    {
+                        checkDigit = checkDigit + (Convert.ToInt32(s[i - 1]) * (10 - i));
+                    }
+                    checkDigit = 11 - (checkDigit % 11);
+                    if ((checkDigit >= 10))
+                        checkDigit = 0;
+                    if ((checkDigit == Convert.ToInt32(s[8])))
+                        functionReturnValue = true;
+                }
+            }
+            return functionReturnValue;
+        }
+    }
+}
--- ValidationTaxPayerCard/ValidationTaxPayerCard-CLI/Program.cs
+++ ValidationTaxPayerCard/ValidationTaxPayerCard-CLI/Program.cs
@@ -6,22 +6,107 @@ namespace ValidationTaxPayerCard_CLI
 
     public static class Program
     {
+        private const string HELP = "help";
+        private const string EXIT = "exit";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to TaxPayerVerify. Type in a command.");
-            while (true)
+            bool exit = true;
+
+            if (args == null)
             {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Console.WriteLine(StringsResource.StartProgram);
+
+            while (exit)
+            {
+                Console.WriteLine(StringsResource.InsertNumberToCheck);
                 args = new[] { Console.ReadLine() };
-                if (args.Contains("/h") || args.Contains("help") || args.Contains("--help"))
+
+                if (args.Contains(EXIT))
+                {
+                    exit = false;
+                }
+
+                if (args.Contains(HELP))
                 {
                     DisplayHelp(args);
+                }
+                else
+                {
+                    string str = String.Format("{0}", args[0]);
+                    if (IsValidContrib(str))
+                    {
+                        Console.WriteLine(StringsResource.ValidNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine(StringsResource.InvalidNumber);
+                    }
                 }
             }
         }
 
         public static void DisplayHelp(string[] args)
         {
-            Console.WriteLine("===== SOME MEANINGFULL HELP ==== ");
+
+            Console.WriteLine(StringsResource.Help);
+        }
+
+        public static bool IsValidContrib(string Contrib)
+        {
+            bool functionReturnValue = false;
+            string[] s = new string[9];
+
+            if (Contrib.Length < 9 || Contrib.Length > 9)
+            {
+                return false;
+            }
+
+            s[0] = Convert.ToString(Contrib[0]);
+            s[1] = Convert.ToString(Contrib[1]);
+            s[2] = Convert.ToString(Contrib[2]);
+            s[3] = Convert.ToString(Contrib[3]);
+            s[4] = Convert.ToString(Contrib[4]);
+            s[5] = Convert.ToString(Contrib[5]);
+            s[6] = Convert.ToString(Contrib[6]);
+            s[7] = Convert.ToString(Contrib[7]);
+            s[8] = Convert.ToString(Contrib[8]);
+
+            if (Contrib.Length == 9)
+            {
+                string c = s[0];
+
+                if (c == null)
+                {
+                    throw new ArgumentNullException(nameof(Contrib));
+                }
+
+                if (s[0] == "1" || s[0] == "2" || s[0] == "5" || s[0] == "6" || s[0] == "9")
+                {
+                    long checkDigit = Convert.ToInt32(c) * 9;
+                    int i;
+                    for (i = 2; i <= 8; i++)
+                    {
+                        checkDigit = checkDigit + (Convert.ToInt32(s[i - 1]) * (10 - i));
+                    }
+                    checkDigit = 11 - (checkDigit % 11);
+                    if ((checkDigit >= 10))
+                        checkDigit = 0;
+                    try
+                    {
+                        if ((checkDigit == Convert.ToInt32(s[8])))
+                            functionReturnValue = true;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(StringsResource.UsedInvalidCharsError);
+                    }
+                }
+            }
+            return functionReturnValue;
         }
 
     }
